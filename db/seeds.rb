dd12a4d6cd53d3de596745e5f72f2fee36c6922f
Hamza Triqui <triqui.hamza@gmail.com>
2022-11-22 04:18:04 +0100

    API seed OMDB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,40 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
 User.destroy_all
 Movie.destroy_all
-puts"destroying..."
+
+# puts"destroying..."
 
 hamza = User.create(first_name: "Hamza", last_name:"Triqui", email:"test@g.com", password:"123456")
-movie1 = Movie.create(title: "Hamza", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
-movie1.user = hamza
-movie1.save
+hamza.save
+puts "destroying..."
 
-titanic = Movie.create(title: "titanic", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
-titanic.user = hamza
-titanic.save
+require "json"
+require "open-uri"
 
-indiana = Movie.create(title: "indiana", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
-indiana.user = hamza
-indiana.save
+query1 = "sun"
+url = "http://www.omdbapi.com/?s=$#{query1}&apikey=d68d3c66"
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
 
-tarzan = Movie.create(title: "tarzan", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
-tarzan.user = hamza
-tarzan.save
-
-jungle = Movie.create(title: "jungle", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
-jungle.user = hamza
-jungle.save
-
-twilight = Movie.create(title: "twilight", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
-twilight.user = hamza
-twilight.save
-
-
+movies["Search"].each do |movie|
+  movie = Movie.new(title: movie["Title"], description: "none", poster: movie["Poster"], price: rand(1..1000), movie_type: ["Drama", "Comedy", "Superhero"].sample, address: "Paris", release_date: movie["Year"])
+  movie.user = hamza
+  movie.save
+end
 
 puts "seed is done"
+
+# movie1 = Movie.create(title: "Hamza", description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+# movie1.user = hamza
+# movie1.save
+
+# titanic = Movie.create(title: "titanic", description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+# titanic.user = hamza
+# titanic.save
+
+# indiana = Movie.create(title: "indiana", description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+# indiana.user = hamza
+# indiana.save
+
+# tarzan = Movie.create(title: "tarzan", description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+# tarzan.user = hamza
+# tarzan.save
+
+# jungle = Movie.create(title: "jungle", description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+# jungle.user = hamza
+# jungle.save
+
+# twilight = Movie.create(title: "twilight", description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+# twilight.user = hamza
+# twilight.save
+
+# puts "seed is done"
 # Movie.create([title: "Hamza"])
 # user = User.create(first_name: "test", last_name:"Triqui", email:"test@g.com", password:"123456")
-# movie2 = Movie.new(title: "movie 2", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+# movie2 = Movie.new(title: "movie 2", description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
 # movie2.user = user
 # movie2.save
