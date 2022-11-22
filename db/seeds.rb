396118f563e8d0352f75766e134529c7f02fd037
maxence ravau <maxenceravau@gmail.com>
2022-11-22 10:50:42 +0100

    add the movie1 to the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# movie1 = Movie.create(title: "Hamza", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
-Movie.create([title: "Hamza"])
+user = User.create(first_name: "Hamza", last_name:"Triqui", email:"test@g.com", password:"123456")
+movie1 = Movie.new(title: "Hamza", release_date: 1994, description: "test", poster: "img", price: 1000, movie_type: "Drama", address: "Paris 11")
+movie1.user = user
+movie1.save
+# Movie.create([title: "Hamza"])
