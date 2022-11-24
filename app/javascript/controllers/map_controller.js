a218f0481190b191d7732f7a10a93937116c8afb
maxence ravau <maxenceravau@gmail.com>
2022-11-24 17:11:22 +0100

    carrousel updated

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -11,7 +11,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/maxenceravau/clav9ci2800cg14ld8j66oi1v"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
