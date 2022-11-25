66c39bbfef813a850c5ca5e3620523bf657b2540
nafti ladhiri <ladhiri.nafti@gmail.com>
2022-11-25 12:49:17 +0100

    validé avec validation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,13 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "bootstrap"
+
+
+const button = document.querySelector('#btn');
+
+button.addEventListener('click', (e) => {
+  e.target.classList.add('disabled');
+  e.target.classList.add('btn-gradient2');
+  e.target.classList.remove('btn-gradient');
+  e.target.innerText = 'Validated by the director';
+});
